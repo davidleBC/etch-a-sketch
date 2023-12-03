61161506a7731434a4a84d9bf27ccc395fe3a34e
DavidLeBC <david.hoang.le@gmail.com>
2023-12-02 22:06:07 -0800

    html, css, js starting file create div

---
+++ script.js
@@ -0,0 +1,11 @@
+let contain = document.querySelector('#container');
+const newDiv = document.createElement('div');
+newDiv.textContent = "This worked!";
+
+contain.appendChild(newDiv);
+
+/*
+for (let i = 0; i <16; i++) {
+    contain.appendChild(newDiv);
+}
+*/
