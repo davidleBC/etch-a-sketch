96f56776c15760795836f3cdf54a50a0ffcf0f38
DavidLeBC <david.hoang.le@gmail.com>
2023-12-02 23:00:34 -0800

    Loop to create multiple divs

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
 let contain = document.querySelector('#container');
-const newDiv = document.createElement('div');
-newDiv.textContent = "This worked!";
 
-contain.appendChild(newDiv);
-
-/*
 for (let i = 0; i <16; i++) {
-    contain.appendChild(newDiv);
-}
-*/
+    const newDiv = document.createElement('div');
+    newDiv.textContent = "This worked!";
+    contain.appendChild(newDiv)
+};
